2805bd2bb9ecee38cc74bf23feffa65d6d91efcb
hongthanh96 <hongthanh010896@gmail.com>
2020-05-06 15:58:01 +0700

    bt-hienthidaura

---
+++ hienthidaura.js
@@ -0,0 +1,12 @@
+function function1(){
+    document.write('Xin chào!')
+}
+function function2(){
+    console.log('Xin chào!')
+}
+function function3(){
+    alert('Xin Chào!')
+}
+function function4(){
+    document.getElementById("demo").innerHTML="Xin Chào!"
+}
