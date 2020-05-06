853daa81403f9e49583b48088e30a67363921355
hongthanh96 <hongthanh010896@gmail.com>
2020-05-06 15:05:15 +0700

    bổ sung js

---
+++ javascipt.js
@@ -0,0 +1,5 @@
+function myfustion(){
+    let num1 = parseInt (document.getElementById("num1").value);
+    let num2 = parseInt (document.getElementById("num2").value);
+    document.getElementById("kq").innerHTML= (num1+num2)
+}
